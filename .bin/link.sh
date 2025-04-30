f03de1b569692f4c2888d6a431de7f87f39e207a
azegame <a.masashi218@gmail.com>
2025-05-01 03:51:09 +0900

    feat: cursor, vscodeの拡張機能対応、link.sh追加

---
+++ .bin/link.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+echo "Create a symbolic link for dotfiles..."
+for dotfile in "${SCRIPT_DIR}"/.??* ; do
+    [[ "$dotfile" == "${SCRIPT_DIR}/.git" ]] && continue
+    [[ "$dotfile" == "${SCRIPT_DIR}/.github" ]] && continue
+    [[ "$dotfile" == "${SCRIPT_DIR}/.DS_Store" ]] && continue
+
+    ln -fnsv "$dotfile" "$HOME"
+done
+
+source ~/.zshrc
---
+++ editor/vscode.sh
@@ -0,0 +1,31 @@
+#!/bin/zsh
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+VSCODE_SET_DIR="${HOME}/Library/Application Support/Code/User"
+
+# Link settings.json
+if [ -L "${VSCODE_SET_DIR}/settings.json" ]; then
+  echo "Linking settings.json to vscode..."
+  ln -fsvn "${SCRIPT_DIR}/settings.json" "${VSCODE_SET_DIR}/settings.json"
+else
+  echo "settings.json is not found."
+  exit 1
+fi
+
+# Install extensions using the code command
+if command -v code >/dev/null 2>&1; then
+  echo "Installing extensions..."
+
+  installed=$(code --list-extensions)
+
+  cat "${SCRIPT_DIR}/extensions" | while read -r ext; do
+    if ! echo "$installed" | grep -qx "$ext"; then
+      echo "Installing $ext..."
+      code --install-extension "$ext"
+    else
+      echo "$ext is already installed, skipping."
+    fi
+  done
+else
+  echo "Code command not found."
+fi
